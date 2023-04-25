0239e64e1beabd9d261dd4985a0557dc52ac2e6f
impronen <impronen@gmail.com>
2023-04-25 17:34:42 +0300

    basic features complete

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Linked List
 
 My attempt at linked list for [The Odin Project](https://www.theodinproject.com/lessons/javascript-linked-lists) exercise
+
+The linked list is implemented with a class construct and using a subclass for node creation.
--- linkedList.js
+++ linkedList.js
@@ -4,7 +4,6 @@ class linkedList {
   constructor() {
     this.headOfList = null;
   }
-  prepend(value) {}
   append(value) {
     let newNode = new Node(value);
     if (this.headOfList === null) {
@@ -15,12 +14,66 @@ class linkedList {
       temp.nextNode = newNode;
     }
   }
-
-  size() {}
-  head() {}
-  tail() {}
-  at(index) {}
-  pop() {}
+  prepend(value) {
+    let temp = null;
+    if (this.headOfList != null) {
+      temp = this.headOfList;
+    }
+    this.headOfList = new Node(value);
+    this.headOfList.nextNode = temp;
+  }
+  size() {
+    let temp = this.headOfList;
+    let count = 0;
+    while (temp.nextNode != null) {
+      count++;
+      temp = temp.nextNode;
+    }
+    return count + 1;
+  }
+  head() {
+    return this.headOfList.value;
+  }
+  tail() {
+    let temp = this.headOfList;
+    while (temp.nextNode != null) temp = temp.nextNode;
+    return temp.value;
+  }
+  at(index) {
+    let temp = this.headOfList;
+    for (let i = 0; i < index; i++) {
+      temp = temp.nextNode;
+      if (temp === null) return "There is nothing at that index mate";
+    }
+    return temp.value;
+  }
+  pop() {
+    let currentNode = this.headOfList;
+    let previousNOde = null;
+    while (currentNode.nextNode != null) {
+      previousNOde = currentNode;
+      currentNode = currentNode.nextNode;
+    }
+    previousNOde.nextNode = null;
+  }
+  find(value) {
+    let index = 0;
+    let temp = this.headOfList;
+    while (temp != null) {
+      if (temp.value === value) return true;
+      temp = temp.nextNode;
+    }
+    return false;
+  }
+  toString() {
+    let temp = this.headOfList;
+    let outputString = "";
+    while (temp != null) {
+      outputString += `(${temp.value}) => `;
+      temp = temp.nextNode;
+    }
+    return (outputString += "null");
+  }
 }
 
 class Node extends linkedList {
@@ -32,6 +85,11 @@ class Node extends linkedList {
 }
 
 const list = new linkedList();
-list.append("OK");
-list.append("OK 2");
+
+list.append("This was added first to the end");
+list.append("Then this");
 console.log(list);
+list.prepend("A new head was added");
+list.append("Then we put some stuff at the end");
+console.log(list);
+console.log(list.toString());
